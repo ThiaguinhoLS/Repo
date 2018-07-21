fd583e9784d16980aa76a23c5e84e1d7cb2939be
ThiaguinhoLS <tthiaguinho638@gmail.com>
2018-07-20 23:33:24 -0300

    Basic coroutine

--- coroutine.py
+++ coroutine.py
@@ -5,9 +5,14 @@ async def coroutine(value):
     await asyncio.sleep(1)
     return value
 
+# Instância o loop de eventos
 loop = asyncio.get_event_loop()
+# Cria uma task
 task = loop.create_task(coroutine(1))
+# Adiciona uma função de callback a task
 task.add_done_callback(lambda future: loop.stop())
+# Faz o loop de eventos rodar infinitamente
 loop.run_forever()
+# Fecha o loop de eventos
 loop.close()
 
