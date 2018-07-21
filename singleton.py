bccd9911fd7a3e0aff491153e0d74b380ea382b2
ThiaguinhoLS <tthiaguinho638@gmail.com>
2018-07-20 23:07:26 -0300

    Implements design pattern Singleton

--- singleton.py
+++ singleton.py
@@ -14,7 +14,7 @@ class Singleton(object):
 		raise NotImplementedError()
 
 
-class Spam(Singleton):
+ class Spam(Singleton):
 
 	'Classe que implementa o método _init que inicializa instância'
 
@@ -22,23 +22,50 @@ class Spam(Singleton):
 		self.value = value
 
 
-def factory_singleton(_instance = Spam()):
-
-	'Retorna somente uma única instância de Spam definida como argumento padrão'
-
-	return _instance
-
 def test_singleton_instance():
 
 	'Testa se a classe Spam só cria uma única instância'
 	
 	assert Spam(1) is Spam(2)
 
+def factory_singleton(_instance = Spam()):
+
+	'Retorna somente uma única instância de Spam definida como argumento padrão'
+
+	return _instance
+
 def test_factory_singleton():
+
+        'Testa se a factory retorna sempre a mesma instância'
+
 	assert factory_singleton() is factory_singleton()
+
+
+class Singleton(object):
+
+    def __init__(self, klass):
+        self._klass = klass
+
+    def __call__(self, *args, **kwargs):
+        if not hasattr(self, '_instance'):
+            self._instance = self._klass(*args, **kwargs)
+        return self._instance
+
+
+def test_singleton_decorator():
+    
+    @Singleton
+    class Spam(object):
+
+        pass
+
+    assert Spam() is Spam()
+
 
 def main():
 	test_singleton_instance()
+        test_factory_singleton()
+        test_singleton_decorator()
 
 if __name__ == '__main__':
 	main()
