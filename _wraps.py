8fce89c536059a23c7e9e875f54e42b201751394
ThiaguinhoLS <tthiaguinho638@gmail.com>
2018-07-20 22:38:13 -0300

    Implements wraps for library functools

---
+++ _wraps.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+def _wraps(func):
+    def _wrapper(deco):
+        deco.__name__ = func.__name__
+        deco.__doc__ = func.__doc__
+        return deco
+    return _wrapper
+
+def decorator(func):
+    @_wraps(func)
+    def _wrapper(message):
+        func(message)
+    return _wrapper
+
+@decorator
+def show_message(message):
+    print(message)
+
