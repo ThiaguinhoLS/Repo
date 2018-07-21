bb17c3210190c111b829591e6e02f082f9af23c2
ThiaguinhoLS <tthiaguinho638@gmail.com>
2018-07-20 23:48:22 -0300

    Design pattern Singleton

--- singleton.py
+++ singleton.py
@@ -8,43 +8,43 @@
 
 class Singleton(object):
 
-	'Classe que implementa o padrão de projeto Singleton'
+    'Classe que implementa o padrão de projeto Singleton'
 
-	def __new__(klass, *args, **kwargs):
-		if not hasattr(klass, '_instance'):
-			klass._instance = object.__new__(klass)
-			klass._instance._init(*args, **kwargs)
-		return klass._instance
+    def __new__(klass, *args, **kwargs):
+	if not hasattr(klass, '_instance'):
+	    klass._instance = object.__new__(klass)
+	    klass._instance._init(*args, **kwargs)
+	return klass._instance
 
-	def _init(self, *args, **kwargs):
-		raise NotImplementedError()
+    def _init(self, *args, **kwargs):
+	raise NotImplementedError()
 
-
- class Spam(Singleton):
-
-	'Classe que implementa o método _init que inicializa instância'
-
-	def _init(self, value = None):
-		self.value = value
 
 
 def test_singleton_instance():
 
-	'Testa se a classe Spam só cria uma única instância'
-	
-	assert Spam(1) is Spam(2)
+    'Testa se a classe Spam só cria uma única instância'
+
+    class Spam(Singleton):
+
+    'Classe que implementa o método _init que inicializa instância'
+
+        def _init(self, value = None):
+	    self.value = value
+
+    assert Spam(1) is Spam(2)
 
 def factory_singleton(_instance = Spam()):
 
-	'Retorna somente uma única instância de Spam definida como argumento padrão'
+    'Retorna somente uma única instância de Spam definida como argumento padrão'
 
-	return _instance
+    return _instance
 
 def test_factory_singleton():
 
     'Testa se a factory retorna sempre a mesma instância'
 
-	assert factory_singleton() is factory_singleton()
+    assert factory_singleton() is factory_singleton()
 
 
 class Singleton(object):
@@ -69,7 +69,7 @@ def test_singleton_decorator():
 
 
 def main():
-	test_singleton_instance()
+    test_singleton_instance()
     test_factory_singleton()
     test_singleton_decorator()
 
