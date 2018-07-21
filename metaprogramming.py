141a3a33287de43b76430c1ed537513923fd1ced
ThiaguinhoLS <tthiaguinho638@gmail.com>
2018-07-20 22:22:56 -0300

    Metaprogramming in Python

---
+++ metaprogramming.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class Meta(type):
+
+    def __new__(mcs, name, bases, dct):
+        print(f'Classe instanciada {name}')
+        klass = super().__new__(mcs, name, bases, dct)
+        return klass
+
+
+class Spam(metaclass = Meta):
+
+    pass
+
