65e0e70bfe73d846ef2bff4c4fb3b16aa655f1f0
ThiaguinhoLS <tthiaguinho638@gmail.com>
2018-07-28 14:49:41 -0300

    Cache com lru_cache

--- _lru_cache.py
+++ _lru_cache.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
+from functools import lru_cache
+from time import time
 
-class lru_cache(object):
 
-    pass
+def fat(x): # Time: 0.00006676
+        value = 1
+        for i in range(1, x + 1):
+            value *= i
+        return value
 
+
+@lru_cache(maxsize=None)
+def fat_cache(x):
+        value = 1
+        for i in range(1, x + 1):
+            value *= i
+        return value
+
+
+def main():
+    funcs = (fat, fat_cache)
+    for func in funcs:
+        start = time()
+        for value in (10, 20, 30):
+            print(func(value))
+    print('Time: {}'.format(time() - start))
